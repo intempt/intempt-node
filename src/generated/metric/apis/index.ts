d29ce1483cf055c768fdb42cb807716e49e8c374
Koray <akbakirkoray@gmail.com>
2023-10-09 13:41:13 +0300

    Update index.ts

--- src/generated/metric/apis/index.ts
+++ src/generated/metric/apis/index.ts
@@ -1,8 +1,3 @@
 /* tslint:disable */
 /* eslint-disable */
-// export * from './CatalogsApi';
-// export * from './EventsApi';
 export * from './GrowthBoardsApi';
-// export * from './MetricsApi';
-// export * from './SegmentsApi';
-// export * from './UsersAttributeValuesApi';
