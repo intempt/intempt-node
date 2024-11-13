a706a79d88a03532f5adb898c5a9de0611947cd6
beska0013 <gugushvili.beso0013@gmail.com>
2024-11-13 19:21:47 +0400

    add: product validation

--- src/SDK.ts
+++ src/SDK.ts
@@ -78,23 +78,51 @@ export class SDK {
     }
 
     async productAdd(profileId: string, productId:String, quantity:number){
-        await this.trackingClient.productTrack('Product Add', profileId, [
-            {productId: productId, quantity: quantity}
-        ])
+        if(!productId || !quantity && quantity <= 0 || !!profileId){
+            console.warn('product add request params are incorrect')
+            return
+        }
+
+        try{
+            await this.trackingClient.productTrack('Product Add', profileId, [
+                {productId: productId, quantity: quantity}
+            ])
+        }
+        catch (e:any){
+            console.warn(`product request error: ${e.message}`)
+            return
+        }
     }
 
     async productOrdered(profileId: string, products:{productId:String, quantity:number}[]){
-        if (products) {
-            await this.trackingClient.productTrack('Product Ordered', profileId, products)
+        if (profileId && products && products.length > 0 && products.every(product => product.productId && product.quantity)) {
+            try{
+                await this.trackingClient.productTrack('Product Ordered', profileId, products)
+            }
+            catch(e:any){
+                console.warn(`product request error: ${e.message}`)
+                return
+            }
         } else {
             console.warn('product ordered request params are incorrect')
         }
     }
 
     async productView(profileId: string, productId:String){
-        await this.trackingClient.productTrack('Product View', profileId, [
-            {productId: productId}
-        ])
+        if(!profileId || !productId){
+            console.warn('product view request params are incorrect')
+            return
+        }
+        try{
+            await this.trackingClient.productTrack('Product View', profileId, [
+                {productId: productId}
+            ])
+        }
+        catch(e:any){
+            console.warn(`product request error: ${e.message}`)
+            return
+        }
+
     }
 
     async choosePersonalizationsByGroups(profileId: string, groups?: string[]): Promise<any> {
